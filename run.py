0bc3d7bcb80c86969039c44180dee8741dcf777a
SumedhDawadi <sumedh,dawadi101@gmail.com>
2021-03-12 20:17:45 +0545

    Banner

--- run.py
+++ run.py
@@ -1,3 +1,27 @@
+import pyfiglet
+
+ascii_banner = pyfiglet.figlet_format(\
+'''
+Message Boomer: 
+BY: SumedhI Love you!
+I Love you!
+I Love you!
+I Love you!
+I Love you!
+I Love you!
+I Love you!
+I Love you!
+I Love you!
+I Love you!
+I Love you!
+I Love you!
+   ''')
+   
+
+I Love you!
+
+
+print(ascii_banner)
 import pyautogui
 import time
 while True:
