3b8123a10ec0e58e56a9e74ec6875a8ed2d31f0b
SumedhDawadi <sumedh,dawadi101@gmail.com>
2021-03-12 18:39:30 +0545

    MESSAGE_BOOMER

---
+++ run.py
@@ -0,0 +1,9 @@
+import pyautogui
+import time
+while True:
+    pyautogui.typewrite("I Love you!")                                 #@AUTHOR: SUMEDH DAWADI
+    time.sleep(1)
+    pyautogui.press('enter')
+
+
+
